f196aa5ef7b0023a476571899489f7692c7082a3
Patrick Schlarmann <pschlarm@posteo.de>
2022-11-12 18:01:16 +0100

    fix function project

--- Api/Message.cs
+++ Api/Message.cs
@@ -1,5 +1,3 @@
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -15,7 +13,7 @@ public static class Message
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
         ILogger log)
-    {
+    {        
         log.LogInformation("C# HTTP trigger function processed a request.");
 
         string name = req.Query["name"];
